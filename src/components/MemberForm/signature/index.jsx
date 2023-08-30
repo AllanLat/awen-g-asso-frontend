a4aabaec90f5972a3b9c49388f0e34daec488126
FullStackMan <allan.latruffe@gmail.com>
2023-08-30 16:59:31 +0200

    ajout de la date

--- src/components/MemberForm/signature/index.jsx
+++ src/components/MemberForm/signature/index.jsx
@@ -20,6 +20,12 @@ const generatePDFContent = (image, droitImage, nomPRenom) => {
   // Générez le contenu du PDF à partir des données fournies
 
   console.log(nomPRenom);
+  const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    const formattedDate = `${day}/${month}/${year}`;
 
   return (
     <Document>
@@ -29,6 +35,7 @@ const generatePDFContent = (image, droitImage, nomPRenom) => {
           <Text>Je certifie que mes informations renseignées sont exactes</Text>
           <Text style={{ marginBottom: 40 }}>{droitImage ? "J'accepte le droit à l'image" : "Je n'accepte pas le droit à l'image"}</Text>
           <Text style={{ marginBottom: 10 }}>{nomPRenom}</Text>
+          <Text style={{ marginBottom: 10 }}>{formattedDate}</Text>
           <Image style={styles.image} src={image} />
         </View>
       </Page>
