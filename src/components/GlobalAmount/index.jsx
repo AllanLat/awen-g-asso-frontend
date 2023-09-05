ee6a8b8a84c9be29a43d49f8dce84aabe896fdcf
FullStackMan <allan.latruffe@gmail.com>
2023-09-05 13:09:03 +0200

    modification des console.log

--- src/components/GlobalAmount/index.jsx
+++ src/components/GlobalAmount/index.jsx
@@ -1,14 +1,10 @@
 import './index.css'
 
 const GlobalAmount = ({amount, total}) => {
-
-    console.log(total)
     if(!total) {
         total = 0
     }
-    
-    const isNegative = amount < total
-    console.log(isNegative);
+    const isNegative = amount < total;
     return(
         <div className='amount'>
             <p className={isNegative ? "detail-amount red" : "detail-amount green"}>{amount}€</p>
